171331d6902fd6691298a819e8b81bfcbf66cce6
Sergei Barbarash <sgt.sgt@gmail.com>
2025-03-05 18:52:25 +0300

    light theme for neovide

--- init.lua
+++ init.lua
@@ -780,7 +780,11 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-day'
+      if vim.g.neovide then
+        vim.cmd.colorscheme 'tokyonight-day'
+      else
+        vim.cmd.colorscheme 'tokyonight'
+      end
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
